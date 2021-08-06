4d9e824a9d1bb9e2d279831b8794db4242c81204
Farhan <arriqaaq@yahoo.com>
2021-08-07 02:13:59 +0530

    Update README.md

--- main.go
+++ main.go
@@ -5,10 +5,8 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"reflect"
-	"strconv"
 	"strings"
 	"syscall"
 
@@ -75,17 +73,14 @@ type Reference struct {
 func unmarshalYAML(in io.Reader) (interface{}, error) {
 	b, err := ioutil.ReadAll(in)
 	if err != nil {
-		log.Println(err)
 		return nil, err
 	}
 	if len(b) == 0 {
-		log.Println(err)
 		return nil, errEmptyYaml
 	}
 
 	var i interface{}
 	if err := yaml.Unmarshal(b, &i); err != nil {
-		log.Println(err)
 		return nil, err
 	}
 
@@ -110,28 +105,27 @@ func newYQ() *yq {
 }
 
 func (g *yq) run(i interface{}) error {
-	g.tree.UpdateView(g, i)
-	g.tree.SetKeybindings(g)
+	g.tree.updateView(g, i)
+	g.tree.setKeybindings(g)
 
 	grid := tview.NewGrid().AddItem(g.tree, 0, 0, 1, 1, 0, 0, true)
 	g.pages.AddAndSwitchToPage("main", grid, true)
 
 	if err := g.app.SetRoot(g.pages, true).Run(); err != nil {
-		log.Println(err)
 		return err
 	}
 
 	return nil
 }
 
-func (g *yq) Modal(p tview.Primitive, width, height int) tview.Primitive {
+func (g *yq) modal(p tview.Primitive, width, height int) tview.Primitive {
 	return tview.NewGrid().
 		SetColumns(0, width, 0).
 		SetRows(0, height, 0).
 		AddItem(p, 1, 1, 1, 1, 0, 0, true)
 }
 
-func (g *yq) Search() {
+func (g *yq) search() {
 	pageName := "search"
 	if g.pages.HasPage(pageName) {
 		g.pages.ShowPage(pageName)
@@ -152,7 +146,7 @@ func (g *yq) Search() {
 			}
 		})
 
-		g.pages.AddAndSwitchToPage(pageName, g.Modal(input, 0, 3), true).ShowPage("main")
+		g.pages.AddAndSwitchToPage(pageName, g.modal(input, 0, 3), true).ShowPage("main")
 	}
 }
 
@@ -160,7 +154,6 @@ func (g *yq) walk(nodes []*tview.TreeNode, text string) []*tview.TreeNode {
 	var newNodes []*tview.TreeNode
 
 	for _, child := range nodes {
-		log.Println(child.GetText())
 		if strings.Contains(strings.ToLower(child.GetText()), strings.ToLower(text)) {
 			newNodes = append(newNodes, child)
 		} else {
@@ -173,25 +166,23 @@ func (g *yq) walk(nodes []*tview.TreeNode, text string) []*tview.TreeNode {
 
 type tree struct {
 	*tview.TreeView
-	root       *tview.TreeNode
-	isExpanded bool
+	root *tview.TreeNode
 }
 
 func newTree() *tree {
 	t := &tree{
-		TreeView:   tview.NewTreeView(),
-		isExpanded: true,
+		TreeView: tview.NewTreeView(),
 	}
 
 	// t.SetBorder(true).SetTitle("yaml tree").SetTitleAlign(tview.AlignLeft)
 	return t
 }
 
-func (t *tree) UpdateView(g *yq, i interface{}) {
+func (t *tree) updateView(g *yq, i interface{}) {
 	g.app.QueueUpdateDraw(func() {
 
-		r := NewRootTreeNode(i)
-		r.SetChildren(t.AddNode(i))
+		r := newRootTreeNode(i)
+		r.SetChildren(t.addNode(i))
 		t.SetRoot(r).SetCurrentNode(r)
 
 		root := *r
@@ -199,7 +190,7 @@ func (t *tree) UpdateView(g *yq, i interface{}) {
 	})
 }
 
-func (t *tree) AddNode(node interface{}) []*tview.TreeNode {
+func (t *tree) addNode(node interface{}) []*tview.TreeNode {
 	var nodes []*tview.TreeNode
 
 	switch node := node.(type) {
@@ -209,23 +200,23 @@ func (t *tree) AddNode(node interface{}) []*tview.TreeNode {
 			switch v.(type) {
 			case map[string]interface{}:
 				objectNode := tview.NewTreeNode("{object}").
-					SetChildren(t.AddNode(v)).SetReference(Reference{ID: id, YAMLType: Object})
+					SetChildren(t.addNode(v)).SetReference(Reference{ID: id, YAMLType: Object})
 				nodes = append(nodes, objectNode)
 			case []interface{}:
 				nodeName := fmt.Sprintf("{array}%d", i)
 				arrayNode := tview.NewTreeNode(nodeName).
-					SetChildren(t.AddNode(v)).SetReference(Reference{ID: id, YAMLType: Array})
+					SetChildren(t.addNode(v)).SetReference(Reference{ID: id, YAMLType: Array})
 				nodes = append(nodes, arrayNode)
 			default:
-				nodes = append(nodes, t.AddNode(v)...)
+				nodes = append(nodes, t.addNode(v)...)
 			}
 		}
 
 	case map[interface{}]interface{}:
 		for k, v := range node {
-			newNode := t.NewNodeWithLiteral(k).
+			newNode := t.newNodeWithLiteral(k).
 				SetColor(tcell.NewHexColor(16711764)).
-				SetChildren(t.AddNode(v))
+				SetChildren(t.addNode(v))
 			r := reflect.ValueOf(v)
 
 			id := uuid.Must(uuid.NewV4()).String()
@@ -258,40 +249,32 @@ func (t *tree) AddNode(node interface{}) []*tview.TreeNode {
 		}
 
 		id := uuid.Must(uuid.NewV4()).String()
-		nodes = append(nodes, t.NewNodeWithLiteral(node).
+		nodes = append(nodes, t.newNodeWithLiteral(node).
 			SetReference(Reference{ID: id, YAMLType: Value, ValueType: valueType}))
 	}
 	return nodes
 }
 
-func (t *tree) NewNodeWithLiteral(i interface{}) *tview.TreeNode {
+func (t *tree) newNodeWithLiteral(i interface{}) *tview.TreeNode {
 	if i == nil {
 		return tview.NewTreeNode("null")
 	}
 	return tview.NewTreeNode(fmt.Sprintf("%v", i))
 }
 
-func (t *tree) SetKeybindings(g *yq) {
+func (t *tree) setKeybindings(g *yq) {
 
 	t.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Rune() {
 		case 's':
-			if t.isExpanded {
-				t.isExpanded = false
-			} else {
-				t.isExpanded = true
-			}
-			t.GetCurrentNode().SetExpanded(t.isExpanded)
+			current := t.GetCurrentNode()
+			current.SetExpanded(!current.IsExpanded())
 		case 'S':
-			if t.isExpanded {
-				t.isExpanded = false
-				t.CollapseValues(t.GetRoot())
-			} else {
-				t.isExpanded = true
-				t.GetRoot().ExpandAll()
-			}
+			t.GetRoot().ExpandAll()
+		case 'X':
+			t.collapseValues(t.GetRoot())
 		case '/', 'f':
-			g.Search()
+			g.search()
 		case ' ':
 			current := t.GetCurrentNode()
 			current.SetExpanded(!current.IsExpanded())
@@ -303,7 +286,7 @@ func (t *tree) SetKeybindings(g *yq) {
 	})
 }
 
-func (t *tree) CollapseValues(node *tview.TreeNode) {
+func (t *tree) collapseValues(node *tview.TreeNode) {
 	node.Walk(func(node, parent *tview.TreeNode) bool {
 		ref := node.GetReference().(Reference)
 		if ref.YAMLType == Value {
@@ -317,24 +300,7 @@ func (t *tree) CollapseValues(node *tview.TreeNode) {
 	})
 }
 
-func parseValueType(text string) valueType {
-	// if sorround with `"` set string type
-	if strings.HasPrefix(text, `"`) && strings.HasSuffix(text, `"`) {
-		return String
-	} else if text == "null" {
-		return Null
-	} else if text == "false" || text == "true" {
-		return Boolean
-	} else if _, err := strconv.ParseFloat(text, 64); err == nil {
-		return Float
-	} else if _, err := strconv.Atoi(text); err == nil {
-		return Int
-	}
-
-	return String
-}
-
-func NewRootTreeNode(i interface{}) *tview.TreeNode {
+func newRootTreeNode(i interface{}) *tview.TreeNode {
 	r := reflect.ValueOf(i)
 
 	var root *tview.TreeNode
@@ -357,11 +323,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	// set tview tty to stdin
 	os.Stdin = os.NewFile(uintptr(syscall.Stderr), "/dev/tty")
 
 	if err := newYQ().run(input); err != nil {
-		log.Println(err)
 		os.Exit(1)
 	}
 	os.Exit(0)
